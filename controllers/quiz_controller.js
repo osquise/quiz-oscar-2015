d52ec2956e12b62ebd7ddf42eb568401c5915a56
osquise <osquise@ingenieros.com>
2015-07-19 22:46:10 -0500

    Desarrollo Rama creditos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -12,3 +12,7 @@ exports.answer = function(req, res){
         res.render('quizes/answer', {respuesta: 'Incorrecto ;('});
     }
 }
+
+exports.author = function(req, res){
+    res.render('quizes/author', {autor: 'Oscar Quiroga'})
+}
