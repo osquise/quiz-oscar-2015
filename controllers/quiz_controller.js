77665854023328742b51ad4d94c8c347d5547269
osquise <osquise@ingenieros.com>
2015-08-03 15:08:09 -0500

    Borrar Pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -97,3 +97,10 @@ exports.update = function(req, res) {
             }     // Redirección HTTP a lista de preguntas (URL relativo)
 };
 
+// DELETE /quizes/:id
+exports.destroy = function(req, res) {
+    req.quiz.destroy().then( function() {
+        res.redirect('/quizes');
+    }).catch(function(error){next(error)});
+};
+
